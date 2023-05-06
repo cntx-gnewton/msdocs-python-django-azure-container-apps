1f60a998fd37d378759a4c88393407cc6a5bb8fb
cntx-gnewton <gnewton@cosnetix.com>
2023-05-06 16:50:59 -0500

    added tutorial.txt url

---
+++ azureproject/__init__.py
@@ -0,0 +1,3 @@
+# az acr build --registry cntxdemoRegistry \
+# --resource-group pythoncontainer-rg \ 
+# --image pythoncontainer:latest https://github.com/cntx-gnewton/msdocs-python-django-azure-container-apps.git
--- restaurant_review/templatetags/restaurant_extras.py
+++ restaurant_review/templatetags/restaurant_extras.py
@@ -2,7 +2,6 @@ from django import template
 
 register = template.Library()
 
-
 @register.inclusion_tag('restaurant_review/star_rating.html')
 def star_rating(avg_rating, review_count):    
     stars_percent = round((avg_rating / 5.0) * 100) if review_count > 0 else 0
